833686723bfde7f5775a830a15d9e33798ee49d0
CH4rnel <frankmozes777lucky@gmail.com>
2026-09-01 07:29:55 +0300

    feat(tools): Built-in tools package

---
+++ glados/tools/builtin/__init__.py
@@ -0,0 +1,11 @@
+# ♃ ☿ 𓂀  OMNISSIAH CONFIG LAYER 𓂀  ☿ ♃
+
+"""
+Built-in tools package for GLaDOS_DAEMON-SYSTEM.
+"""
+
+from glados.tools.builtin.system_info import SystemInfoTool
+
+__all__ = [
+    "SystemInfoTool",
+]
