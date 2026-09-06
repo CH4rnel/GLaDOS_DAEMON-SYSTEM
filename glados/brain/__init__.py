dafb0e7035c1adc5ff7c3368bba0fbb7f5900533
CH4rnel <frankmozes777lucky@gmail.com>
2026-09-06 06:48:07 +0300

    refactor(brain): 3 update imports to use new brain.models module

--- glados/brain/__init__.py
+++ glados/brain/__init__.py
@@ -1,7 +1,18 @@
 # ♃ ☿ 𓂀  OMNISSIAH CONFIG LAYER 𓂀  ☿ ♃
 
-from .engine import BrainEngine
+"""
+Brain package for GLaDOS_DAEMON-SYSTEM.
+"""
+
+from glados.brain.models import TaskInput, ExecutionResult
+from glados.brain.planner import Planner, Plan, PlanStep
+from glados.brain.engine import BrainEngine
 
 __all__ = [
+    "TaskInput",
+    "ExecutionResult",
+    "Planner",
+    "Plan",
+    "PlanStep",
     "BrainEngine",
 ]
