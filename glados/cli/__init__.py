bd027580da6e6f42d6ebcf954fdf286190e67699
CH4rnel <frankmozes777lucky@gmail.com>
2026-09-13 23:45:50 +0300

    cli __init__.py

---
+++ glados/cli/__init__.py
@@ -0,0 +1,10 @@
+# ♃ ☿ 𓂀 OMNISSIAH CODE LAYER 𓂀 ☿ ♃
+
+"""
+CLI package for GLaDOS_DAEMON-SYSTEM.
+Provides command-line interface for interacting with the daemon.
+"""
+
+from glados.cli.main import app
+
+__all__ = ["app"]
